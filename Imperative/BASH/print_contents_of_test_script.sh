ab8ab56e159c592c9ab8a6b2a3e81e9898bf4ff9
Tanveer Salim <tanveerasalim@gmail.com>
2018-04-20 17:15:05 -0700

    master

---
+++ Imperative/BASH/print_contents_of_test_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This is the 
+# template you should follow
+# to do a successful
+# for loop
+# for reference, please visit: https://ryanstutorials.net/bash-scripting-tutorial/bash-loops.php
+echo "***************************************************"
+cd /Users/tanveersalim/Desktop/Git/PIC-10A-HW/PIC-10A-HW/roman #Enter directory of test script
+sed -n "$i"p smart_roman_tests.txt
---
+++ Imperative/C/string_experiments/README.md
@@ -0,0 +1,5 @@
+# README
+
+These are the experiments involving 
+
+string concatenation and printing.
