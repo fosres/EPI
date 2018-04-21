531d9569b56ef3716fe01f538523583bf0ed63ad
Tanveer Salim <tanveerasalim@gmail.com>
2018-04-20 17:53:48 -0700

    master

---
+++ Imperative/BASH/comprehensive_roman_test_script.sh
@@ -0,0 +1,10 @@
+#comprehensive_roman_test_script.sh
+
+#Just generates a simple test script
+
+#to comprehensive_roman_test_values.txt
+
+for i in {1..3999}
+do
+	echo "$i" >> comprehensive_roman_test_values.txt
+done
