a5769575f642691f06f0ff65734343600bc4aa12
Tanveer Salim <tanveerasalim@gmail.com>
2018-04-30 21:07:28 -0700

    master

---
+++ sync_all_git_repositories.sh
@@ -0,0 +1,42 @@
+# sync_all_git_repositories
+#
+# !/bin/pwd; printf "\n"; bash
+#
+
+# Copy this file to Git_Advice for safe-keeping
+
+	cp sync_all_git_repositories.sh ~/Desktop/Git/EPI/EPI
+
+# Sync Joel_on_Software
+
+	cd ~/Desktop/Git/Joel_on_Software
+
+	pwd; printf "\n"; bash git_save.sh; printf "\n"; printf "\n"
+
+# Sync PIC
+	
+	cd ~/Desktop/Git/PIC/PIC
+
+	pwd; printf "\n"; bash git_save.sh; printf "\n"; printf "\n"
+
+# Sync MCDB_187AL
+
+	cd ~/Desktop/Git/MCDB_187AL/MCDB-187AL/MCDB\ 187AL/
+
+	pwd; printf "\n"; bash git_save.sh; printf "\n"; printf "\n"
+	
+# Sync Sins-in-Software
+
+	cd ~/Desktop/Git/Sins-in-Software/Sins-in-Software/
+
+	pwd; printf "\n"; bash git_save.sh; printf "\n"; printf "\n"
+
+# Sync EPI
+
+	cd ~/Desktop/Git/EPI/EPI/
+
+	pwd; printf "\n"; bash git_save.sh; printf "\n"; printf "\n"
+
+	 cd ~/Desktop/Git/Git_Tests/Git_Tests
+
+         pwd; printf "\n"; bash git_save.sh; printf "\n"; printf "\n"
