367c84ca61bc5d7c642bfe30869c64e3cdc676e4
tanveerasalim <32973884+tanveerasalim@users.noreply.github.com>
2018-05-30 12:01:55 -0700

    Update test_exercise_bin_add.c

--- Imperative/C/test_exercise_bin_add.c
+++ Imperative/C/test_exercise_bin_add.c
@@ -28,7 +28,7 @@ void c_test_version() // slap the output of this method directly into the main m
       
 }
 
-void cpp_test_version()
+void cpp_test_version() // slap the output directly onto the right panel of diff tool checker
 {
     int x;
     int fr,fs;
