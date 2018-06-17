6fa286d09b092c6ea86796679fc6091fec789412
Tanveer Salim <tanveerasalim@gmail.com>
2018-06-16 22:01:49 -0700

    master

--- C-Companion/exercise_2-5/tests_in_cpp.cpp
+++ C-Companion/exercise_2-5/tests_in_cpp.cpp
@@ -1,6 +1,9 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+using std::cout;
+using std::endl;
 
 int statements(int a, int b)
 {
@@ -29,8 +32,21 @@ int randomized_values()
 
 }
 
+void print_tests()
+{
+
+  for ( int i = 0; i < 10000; i++)
+  {
+
+		cout << "printf(\"%d\\n\",method(" << randomized_values() << "," << randomized_values() << "));" << endl;
+  }
+
+}
+
 int main()
 {
  
+	print_tests();
 
 }
+
