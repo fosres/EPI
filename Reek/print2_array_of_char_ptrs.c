fb5b3994c56d18d2de3bd1d4fb48f5e391b141bd
Tanveer Salim <tanveerasalim@gmail.com>
2018-06-21 18:18:25 -0700

    master

--- Reek/print2_array_of_char_ptrs.c
+++ Reek/print2_array_of_char_ptrs.c
@@ -12,22 +12,13 @@ int main()
 {
 
 	static char * s = "The";
-	static char * t = "stag";
-	static char * u = "is";
-	static char * v = "cool";
-	static char * w = "bro!";
-	static char * x = NULL;
-#if 0
-	char **arr = &s;
-	
-	printf("%s\n",*arr++);
-	printf("%s\n",*arr++);
-	printf("%s\n",*arr++);
-	printf("%s\n",*arr++);
-	printf("%s\n",*arr++);
-	printf("%s\n",*arr);
-#endif
-	print_elements(&s);
+	static char ** arr_p = &s;
+	*++arr_p = "stag";
+	*++arr_p = "is";
+	*++arr_p = "cool";
+	*++arr_p = "bro!";
+	*++arr_p = NULL;
+	print_elements(&arr_p[0]);
 	
 
 	// If we were to make a double indirection (**), we would have to replace %s with a %c and the first chars in arrays [s,x] would be printed.
