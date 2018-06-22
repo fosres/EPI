19cba343d910f76bfbe94ffec24527c046e5cd1c
Tanveer Salim <tanveerasalim@gmail.com>
2018-06-21 21:34:24 -0700

    master

--- Reek/print2_array_of_char_ptrs.c
+++ Reek/print2_array_of_char_ptrs.c
@@ -4,13 +4,15 @@
 int * search_for_char(char ** s, const char key)
 {
 	
-	int pos_arr[strlen(s)]; int * p_zero = pos_arr; 
+	const int len = strlen(s); int pos_arr[len]; int * p_zero = pos_arr; 
 	
 	int * p = pos_arr; 
 
 	char ** s_zero = s;
 
-	while (*s != NULL )
+	int i = 0;
+
+	while (i < len )
 	{
 		
 		if ( strchr(*s,key) != NULL )
@@ -18,6 +20,8 @@ int * search_for_char(char ** s, const char key)
 		else
 		{ *p++ = -1; }
 		s++;
+
+		i++;
 	}
 
 
@@ -40,19 +44,19 @@ int main()
 	
 	char ** arr = &s; 
 	
-	char ** arr_p = arr; 
+	static char ** arr_p = arr; 
 
 	*++arr = "swag";
 	*++arr = "is";
-	*++arr = "cool";
+	*++arr = "cal";
 	*++arr = "bro!";
 	*++arr = "Yay!";
 	*++arr = NULL;
+	
 
-	print_elements(arr_p);
 	
 	int * pos = search_for_char(arr_p,'a');
-
+	
 	for ( int i = 0; i < strlen(arr_p); i++)
 		printf("arr[%d] has key at: %d\n",i,*pos++);
 	// If we were to make a double indirection (**), we would have to replace %s with a %c and the first chars in arrays [s,x] would be printed.
