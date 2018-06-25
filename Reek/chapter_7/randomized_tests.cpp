33fae2ae85fecf4ca881edd2e327060d662bcdea
Tanveer Salim <tanveerasalim@gmail.com>
2018-06-24 19:02:36 -0700

    master

--- Reek/chapter_7/randomized_tests.cpp
+++ Reek/chapter_7/randomized_tests.cpp
@@ -8,32 +8,42 @@ using std::cout;
 using std::endl;
 
 
-long long int random_lld()
+void random_lld_tests()
 {
+
+  long long int random_lld; 
+   
+   /* random number range: [LLONG_MIN,LLONG_MAX] */
+
+  static int i = 0;
+  while (i < 10000 )
+  {
+   static const int signs[2] = {1,-1};
+
+   int sign_index = (0+rand()/(double)RAND_MAX*(1-0)+0.5);
+
+  random_lld = signs[sign_index]*(0+rand()/(double)RAND_MAX*(LLONG_MAX)+0.5);
   
-
-  static const int signs[2] = {1,-1};
-
-  int sign_index = (0+rand()/(double)RAND_MAX*(1-0)+0.5);
-
-  return signs[sign_index]*(0+rand()/(double)RAND_MAX*(LLONG_MAX)+0.5); 
+  cout << random_lld << endl;
   
-  /* random number range: [LLONG_MIN,LLONG_MAX] */
+  i++;
+  }
 
 }
 
-int main(void) {
-  
-  const int NUMBER_OF_TESTS = 10000; 
+int main(void) 
+{
 
-  static int i;
-
-  while (i < NUMBER_OF_TESTS )
+#if 0
+  static int i = 0;
+  while (i < 10000 )
   {
   cout << random_lld() << endl;
   i++;
   }
-  
+#endif
+
+random_lld_tests();
   
 }
 
