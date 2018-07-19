184f4ad892e23626531efcc7e538079343736654
Tanveer Salim <tanveerasalim@gmail.com>
2018-07-19 13:10:02 -0700

    master

---
+++ exercise_5-2.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+void reverse(int a[], const int len)
+{
+	for (int i = 0, j = len-1; i < j; i++, j--)
+	{
+		int temp = a[i];
+
+		a[i] = a[j];
+
+		a[j] = temp;
+	}
+}
+
+void inc_arr(int a[], int len)
+{
+	int IS_NEG = 0;	
+	if ( a[0] < 0)
+	{ IS_NEG = 1; a[0] = -a[0];}
+
+	int temp = 0;
+	
+	int i = 0;
+	while (i < len)
+	{
+		temp = temp * 10 + a[i];
+
+		i++;
+	}
+
+	if (!IS_NEG)
+	{temp++;}
+	else
+	{temp--;}
+
+	i = 0;
+	
+	len = 0;
+
+	do
+	{
+		a[i] = temp%10;
+	
+		i++; len++;
+
+		temp /= 10;
+	}while (temp > 0);
+
+	reverse(a,len);
+
+	if (IS_NEG)
+	{
+		a[0] = -a[0];
+	}
+}
+
+int main()
+{
+	int a[4] = {-3,0,0};
+
+	inc_arr(a,3); // input for len must be actual # of digits initialized in array, NOT sizeof(a)/sizeof(int)
+
+	for ( int i = 0; i < 4 ; i++)
+	{
+		printf("%d\n",a[i]);
+	}
+}
