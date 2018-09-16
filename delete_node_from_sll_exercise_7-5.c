77813a706d5d1000c5683b3a10b3e5b92765f2c7
tanveerasalim <32973884+tanveerasalim@users.noreply.github.com>
2018-09-16 18:23:47 -0500

    Create delete_node_from_sll_exercise_7-5.c

---
+++ delete_node_from_sll_exercise_7-5.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+  struct Node * link;
+
+  int x;
+
+} Node;
+
+void delete_node (Node * rootp)
+{
+  if ( (rootp) == NULL )
+  {
+    return;
+  }
+
+  *(rootp) = *((rootp)->link);
+}
+
+void print_sll(Node * root)
+{
+  while ( root != NULL )
+  {
+    printf("%p's int  is: %d ",root,root->x);
+
+    root = root->link;
+  }
+
+  printf("\n");
+}
+
+int main(void) 
+{
+
+Node * first = (Node *)malloc(sizeof(Node));
+
+first->x = 1;
+
+Node * second = (Node *)malloc(sizeof(Node));
+
+second->x = 2;
+
+Node * third = (Node *)malloc(sizeof(Node));
+
+third->x = 3;
+
+Node * fourth = (Node *)malloc(sizeof(Node));
+
+fourth->x = 4;
+
+first->link = second;
+
+second->link = third;
+
+third->link = fourth;
+
+fourth->link = NULL;
+
+print_sll(first);
+
+delete_node(third);
+
+print_sll(first);
+
+free(first);
+
+free(second);
+
+free(third);
+
+free(fourth);
+}
