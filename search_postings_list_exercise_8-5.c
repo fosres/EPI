844eed64f2d14ebae5f76128b680b3b8ef9ad343
tanveerasalim <32973884+tanveerasalim@users.noreply.github.com>
2018-10-14 10:52:22 -0500

    Create search_postings_list_exercise_8-5.c

---
+++ search_postings_list_exercise_8-5.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+	
+	struct Node * link;
+
+	struct Node * jmp;
+
+	int value;
+
+} Node;
+
+static Node * stack[1000];
+
+static Node ** stack_p = &stack[0]-1;
+
+void push(Node * r);
+
+Node * pop( );
+
+int in_stack(Node * r);
+
+void jmp_first(Node * r)
+{
+	while (!in_stack(r))
+	{
+		printf("%p ",r);
+
+		push(r);
+
+		r = r->jmp;
+
+	}
+
+
+	while (r != NULL)
+	{
+		printf("%p ",r);
+
+		r = r->link;
+
+	}
+
+}
+
+void push(Node * r)
+{
+
+	*++stack_p = r;
+
+}
+
+Node * pop()
+{
+	
+	return *stack_p--;
+
+}
+
+int in_stack(Node * r)
+{
+
+	static Node ** current_p = stack_p;
+
+	while (stack_p > (&stack[0]-1) && pop() != r)
+		;
+
+	stack_p = current_p;
+
+	return (stack_p == (&stack[0]-1)) ? 0 : 1;
+
+}
+
+int main()
+{
+
+
+}
