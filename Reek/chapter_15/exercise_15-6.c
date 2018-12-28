07484a5e4c7e7b0663d990feca63e1a35b146c1f
tanveerasalim@gmail.com <tanveerasalim@gmail.com>
2018-12-28 10:47:04 +0600

    master

--- Reek/chapter_15/exercise_15-6.c
+++ Reek/chapter_15/exercise_15-6.c
@@ -1,8 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <limits.h>
 
 const int BUFFER_SIZE = 100;
+
+int gen_int_file(char * fname) //returns int for exit_status
+{
+    int exit_status = 0; 
+    
+    FILE * fp = fopen(fname,"w+");
+    
+    if ( fp == NULL)
+    {
+        perror(fname);
+        
+        exit_status = EXIT_FAILURE;
+
+    }
+    
+    int line_word_size = 0;
+
+    int i = 0;
+
+    int j = 0;
+
+    while ( i < 2000 )
+    {
+        line_word_size = (int)((double)rand()/RAND_MAX*10.+0.5);  
+        
+        while ( j < line_word_size )
+        {
+            fprintf(fp,"%d ",(int)((double)rand()/RAND_MAX*INT_MAX+0.5)); 
+
+            j++;
+        }
+
+            fprintf(fp,"\n"); 
+
+        i++;
+    }
+    
+    if (fclose(fp) != 0)
+    {
+        perror("fclose");
+
+        exit(EXIT_FAILURE);
+
+    }
+    
+    return exit_status;
+}
+
 
 double avg_line_ints(char * s)
 {
@@ -52,7 +101,7 @@ return sum/num_words;
 
 }
 
-void line_proc(FILE * input)
+void line_proc(FILE * input, FILE * output)
 {
 
     char buffer[BUFFER_SIZE];
@@ -65,7 +114,7 @@ void line_proc(FILE * input)
 
     while (fgets(buffer,BUFFER_SIZE,input) != NULL )
     {
-        printf("%5.2f: %s\n\n",avg_line_ints(buffer),buffer);
+        fprintf(output,"%5.2f: %s\n\n",avg_line_ints(buffer),buffer);
     }
 
 }
@@ -89,9 +138,19 @@ void free_arr(void * arr,int SIZE)
 
 int main(int argc, char ** argv)
 {
-   int exit_status = EXIT_FAILURE; 
+    static char fname[64]; 
     
-    FILE * fp = fopen(argv[1],"r+");
+    printf("Enter desired name of file for generated random ints: "); 
+
+    scanf("%s",&fname[0]);
+    
+    gen_int_file(fname);
+
+    int exit_status = EXIT_FAILURE; 
+    
+    FILE * fp = fopen(fname,"r+");
+
+    FILE * op = fopen(fname,"w+");
 
     if (fp == NULL)
     {
@@ -99,10 +158,25 @@ int main(int argc, char ** argv)
 
         exit_status = EXIT_FAILURE;
     }
+
+    if (op == NULL)
+    {
+        perror("op");
+
+        exit_status = EXIT_FAILURE;
+    }
     
-    line_proc(fp); 
+    line_proc(fp,op); 
 
     if (fclose(fp) != 0)
+    {
+        perror("fclose");
+
+        exit(EXIT_FAILURE);
+
+    }
+
+    if (fclose(op) != 0)
     {
         perror("fclose");
 
