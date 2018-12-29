ca43a8272de05642cd407cddc43c0cef02d5be4c
tanveerasalim@gmail.com <tanveerasalim@gmail.com>
2018-12-29 16:13:02 +0600

    master

---
+++ Reek/chapter_15/fgrep_exercise_15-9.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char * mystrstr(char * s, char *t)
+{
+  if ( *s == '\0' && *s == *t )
+  { return s; }
+
+  if (*t == '\0')
+  { return &s[strlen(s)];}
+
+  char * s_start; char * t_zero = t;
+
+  while (*s != '\0')
+  {
+    if (*s != *t)
+    {
+      s++;
+
+      continue;
+    }
+
+    s_start = s;
+
+    while (*t != '\0' && *s == *t)
+    {
+      s++; t++;
+    }
+
+    if ( (*t != '\0') && (*(s-1) != t[strlen(t_zero)-1]) )
+    {
+      s = s_start; s++; t = t_zero;
+    }
+
+    else
+    {
+      return s_start;
+    }
+  }
+
+  return NULL;
+
+}
+
+
+int main(int argc, char ** argv)
+{
+  int exit_status = EXIT_SUCCESS; 
+   
+   static char buf[1024];
+
+   int linenum = 0;
+
+   char ** argv_end = &argv[0] + argc;
+
+   char * search_str = argv[1];
+   
+   FILE * in = NULL;
+
+   argv = argv+2;
+
+while ( argv < argv_end )
+{
+
+  in = fopen(*argv,"r+"); 
+   
+   if ( in == NULL )
+   {
+        perror(*argv);
+        
+        exit_status = EXIT_FAILURE;
+   }
+
+   linenum = 0;
+    
+    while (fgets(buf,1024,in) != NULL)
+    {
+      
+        linenum++; 
+       
+        if ( mystrstr(buf,search_str) != NULL ) //argv[1] is the search string
+        { 
+            printf("%s Line %d: %s\n",*argv,linenum,buf);
+        
+        }
+        
+    }
+
+    if (fclose(in) != 0)
+    {
+        perror("fclose");
+
+        exit(EXIT_FAILURE);
+    }
+
+argv++;
+
+}
+
+
+return exit_status;
+}
---
+++ Reek/chapter_15/wc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char ** argv)
+{
+
+    const char * flags = "clw";  
+   
+   int exit_status = EXIT_SUCCESS;
+
+   const char * fname[1024] = argv[argc-1];
+
+   return exit_status;
+}
